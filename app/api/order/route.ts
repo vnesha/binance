49b3a1c7d2325dd889d2cb57b1357b99df452bae
vnesha <vnesha@gmail.com>
2023-05-31 16:28:10 +0200

    test

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,18 +1,24 @@
+import { NextResponse } from "next/server";
+
 export async function GET(request: Request) {
-    console.log("Received request:", request);
-    
-    const url = new URL(request.url);
-    console.log("URL:", url);
-    
-    const { searchParams } = url;
+    const { searchParams } = new URL(request.url);
     const symbol = searchParams.get('symbol');
     const quantity = searchParams.get('quantity');
-
-    console.log("Symbol:", symbol);
-    console.log("Quantity:", quantity);
-
-    const redirectUrl = `/api/order?symbol=${symbol}&quantity=${quantity}`;
-    console.log("Redirect URL:", redirectUrl);
+    return NextResponse.json({quantity, symbol});
+//     console.log("Received request:", request);
     
-    return Response.redirect(redirectUrl, 302);
+//     const url = new URL(request.url);
+//     console.log("URL:", url);
+    
+//     const { searchParams } = url;
+//     const symbol = searchParams.get('symbol');
+//     const quantity = searchParams.get('quantity');
+
+//     console.log("Symbol:", symbol);
+//     console.log("Quantity:", quantity);
+
+//     const redirectUrl = `/api/order?symbol=${symbol}&quantity=${quantity}`;
+//     console.log("Redirect URL:", redirectUrl);
+    
+//     return Response.redirect(redirectUrl, 302);
 }
