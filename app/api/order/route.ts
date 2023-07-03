24c6c8e4cdfc1914edb6b03505540653e5f9e4f2
vnesha <vnesha@gmail.com>
2023-07-03 10:57:49 +0200

    03.07.2023 10:57

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,23 +1,38 @@
 import { NextResponse } from "next/server";
-import { openOrder } from "@/app/hooks/useTestOpenOreder";
+import { openOrder } from "@/hooks/useTestOpenOreder";
 
 export async function GET(request: Response) {
-    const { searchParams } = new URL(request.url);
-    const symbol = searchParams.get('symbol');
-    const quantity = searchParams.get('quantity');
-  
-    if (!symbol || !quantity) {
-      return NextResponse.json({ error: "Invalid parameters" });
-    }
-  
-    try {
-      // Pozivanje openOrder funkcije sa podacima iz zahteva
-      const response = await openOrder({ symbol, quantity: Number(quantity) });
-  
-      // Vraćanje odgovora od openOrder funkcije kao JSON
-      return NextResponse.json(response);
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json({ error: "Error processing order" });
-    }
+  const { searchParams } = new URL(request.url);
+  const symbol = searchParams.get('symbol');
+  const quantity = searchParams.get('quantity');
+
+  if (!symbol || !quantity) {
+    return NextResponse.json({ error: "Invalid parameters" });
   }
+
+  try {
+    const response = await openOrder({ symbol, quantity: Number(quantity) });
+    return NextResponse.json(response);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Error processing order" });
+  }
+}
+
+export async function POST(request: Response) {
+  const { searchParams } = new URL(request.url);
+  const symbol = searchParams.get('symbol');
+  const quantity = searchParams.get('quantity');
+
+  if (!symbol || !quantity) {
+    return NextResponse.json({ error: "Invalid parameters" });
+  }
+
+  try {
+    const response = await openOrder({ symbol, quantity: Number(quantity) });
+    return NextResponse.json(response);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Error processing order" });
+  }
+}
