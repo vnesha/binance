68e5f4a440fde4476121625325865b70b5e16adb
vnesha <vnesha@gmail.com>
2023-07-03 11:01:27 +0200

    03.07.23 11:01

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,23 +1,23 @@
 import { NextResponse } from "next/server";
 import { openOrder } from "@/hooks/useTestOpenOreder";
 
-export async function GET(request: Response) {
-  const { searchParams } = new URL(request.url);
-  const symbol = searchParams.get('symbol');
-  const quantity = searchParams.get('quantity');
+// export async function GET(request: Response) {
+//   const { searchParams } = new URL(request.url);
+//   const symbol = searchParams.get('symbol');
+//   const quantity = searchParams.get('quantity');
 
-  if (!symbol || !quantity) {
-    return NextResponse.json({ error: "Invalid parameters" });
-  }
+//   if (!symbol || !quantity) {
+//     return NextResponse.json({ error: "Invalid parameters" });
+//   }
 
-  try {
-    const response = await openOrder({ symbol, quantity: Number(quantity) });
-    return NextResponse.json(response);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Error processing order" });
-  }
-}
+//   try {
+//     const response = await openOrder({ symbol, quantity: Number(quantity) });
+//     return NextResponse.json(response);
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json({ error: "Error processing order" });
+//   }
+// }
 
 export async function POST(request: Response) {
   const { searchParams } = new URL(request.url);
